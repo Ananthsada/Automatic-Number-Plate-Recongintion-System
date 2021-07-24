19f67b23e620a2d8b71bb1fc730af928ab412023
Sadaananth Anbucheliyan <ananth.anbucheliyan@gmail.com>
2021-07-25 00:06:39 +0800

    1. Applied adaptive thresold to the image 2. Applied contours and bounding boxes to the bw imagewq

--- ANPR/ANPR/ANPR.cpp
+++ ANPR/ANPR/ANPR.cpp
@@ -6,12 +6,47 @@
 #include "Filters.h"
 
 #include <iostream>
+#include <vector>
 
 using namespace cv;
 
+void doFilter(Mat& inputImage, Mat& outputImage)
+{
+	//outputImage = applySobel(inputImage);
+	outputImage = applyCanny(inputImage);
+
+	imshow("Filter applied image", outputImage);
+}
+
+void doContour(Mat& inputImage)
+{
+	std::vector<std::vector<Point>> contours;
+	findContours(inputImage, contours, RETR_CCOMP, CHAIN_APPROX_SIMPLE);
+	std::vector<std::vector<Point>> contours_poly(contours.size());
+	std::vector<Rect> boundRect(contours.size());
+	//std::vector<Point2f>centers(contours.size());
+	//std::vector<float>radius(contours.size());
+	for (size_t i = 0; i < contours.size(); i++)
+	{
+		approxPolyDP(contours[i], contours_poly[i], 3, true);
+		boundRect[i] = boundingRect(contours_poly[i]);
+		//minEnclosingCircle(contours_poly[i], centers[i], radius[i]);
+	}
+	Mat drawing = Mat::zeros(inputImage.size(), CV_8UC3);
+	RNG rng(12345);
+	for (size_t i = 0; i < contours.size(); i++)
+	{
+		Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
+		drawContours(drawing, contours_poly, (int)i, color);
+		rectangle(drawing, boundRect[i].tl(), boundRect[i].br(), color, 2);
+		//circle(drawing, centers[i], (int)radius[i], color, 2);
+	}
+	imshow("Contours", drawing);
+}
+
 int main()
 {
-	std::string image_path = samples::findFile("C:\\Study\\ANPR\\TrainingImages\\archive\\in\\in\\Audi-A4-528794d.jpg_0359_0305_0250_0204_0066.png");
+	std::string image_path = samples::findFile("C:\\Study\\ANPR\\TrainingImages\\input\\Audi-A4-528794d.png");
 	std::cout << "Image path " << image_path << "\n";
 	Mat img = imread(image_path, IMREAD_COLOR);
 
@@ -24,16 +59,35 @@ int main()
 	//imshow("Color image", img);
 	Mat _grayImage;
 	cvtColor(img, _grayImage, COLOR_RGB2GRAY);
-	//imshow("Gray image", _grayImage);
+	imshow("Gray image", _grayImage);
+	cv::Size sz = _grayImage.size();
+	std::cout << "Width=" << sz.width << " height=" << sz.height;
 
-	Mat _sobelApplied = applySobel(_grayImage);
-	imshow("Sobel applied image", _sobelApplied);
+	//Mat img_bw = _grayImage > 128;
+	//imshow("B&W", img_bw);
+	//Mat FilteredImage;
+	//doFilter(_grayImage, FilteredImage);
 
-	Mat _cannyApplied = applyCanny(_grayImage);
-	imshow("Canny applied image", _cannyApplied);
+	Mat _thresholdImage;
+	//threshold(_grayImage, _thresholdImage, 190, 255, THRESH_BINARY + THRESH_OTSU);
+	adaptiveThreshold(_grayImage, _thresholdImage, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 25, 0);
+	imshow("Threshold Image", _thresholdImage);
+	
+	//bitwise_not(_thresholdImage, _thresholdImage);
+	//imshow("Reversed Image", _thresholdImage);
 
+	Mat _element = getStructuringElement(MORPH_RECT, Size(2, 2));
+
+	//Mat _erodedImage;
+	//erode(_thresholdImage, _erodedImage, _element);
+	//imshow("Eroded image", _erodedImage);
+	//
+	Mat _dilatedImage;
+	dilate(_thresholdImage, _dilatedImage, _element);
+	imshow("Dilated image", _dilatedImage);
+
+	doContour(_thresholdImage);
 	int k = waitKey(0);
-
 //	if (k == 's')
 //	{
 //		imwrite("starry_night.png", img);
