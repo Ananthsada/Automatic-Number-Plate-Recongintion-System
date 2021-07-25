3015d4f85ac3a2378ad9fdf3f49e40acc4908496
Sadaananth Anbucheliyan <ananth.anbucheliyan@gmail.com>
2021-07-25 22:02:49 +0800

    Use connected components to locate text

--- ANPR/ANPR/ANPR.cpp
+++ ANPR/ANPR/ANPR.cpp
@@ -10,6 +10,9 @@
 
 using namespace cv;
 
+const std::string ImagePath = "C:\\Study\\ANPR\\TrainingImages\\input\\";
+const std::string ImageName = ImagePath + "Audi-A4-528794d.png";
+
 void doFilter(Mat& inputImage, Mat& outputImage)
 {
 	//outputImage = applySobel(inputImage);
@@ -18,19 +21,41 @@ void doFilter(Mat& inputImage, Mat& outputImage)
 	imshow("Filter applied image", outputImage);
 }
 
+void doConnectedComponents(Mat& inputImage)
+{
+	Mat _labelImage;
+	Mat _statImage, _centroidImage;
+	int _nlabels = connectedComponentsWithStats(inputImage, _labelImage, _statImage, _centroidImage);
+
+	for (int i = 0; i < _statImage.rows; i++)
+	{
+		int x = _statImage.at<int>(Point(0, i));
+		int y = _statImage.at<int>(Point(1, i));
+		int w = _statImage.at<int>(Point(2, i));
+		int h = _statImage.at<int>(Point(3, i));
+
+		std::cout << "x=" << x << " y=" << y << " w=" << w << " h=" << h << std::endl;
+
+		Scalar color(255, 0, 0);
+		Rect rect(x, y, w, h);
+		rectangle(inputImage, rect, color);
+
+		size_t size = aspose::ocr::page_rect(image_path.c_str(), buffer, len, x, y, w, h);
+	}
+	imshow("Rectangles", inputImage);
+}
+
 void doContour(Mat& inputImage)
 {
 	std::vector<std::vector<Point>> contours;
 	findContours(inputImage, contours, RETR_CCOMP, CHAIN_APPROX_SIMPLE);
 	std::vector<std::vector<Point>> contours_poly(contours.size());
 	std::vector<Rect> boundRect(contours.size());
-	//std::vector<Point2f>centers(contours.size());
-	//std::vector<float>radius(contours.size());
+
 	for (size_t i = 0; i < contours.size(); i++)
 	{
 		approxPolyDP(contours[i], contours_poly[i], 3, true);
 		boundRect[i] = boundingRect(contours_poly[i]);
-		//minEnclosingCircle(contours_poly[i], centers[i], radius[i]);
 	}
 	Mat drawing = Mat::zeros(inputImage.size(), CV_8UC3);
 	RNG rng(12345);
@@ -39,14 +64,13 @@ void doContour(Mat& inputImage)
 		Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
 		drawContours(drawing, contours_poly, (int)i, color);
 		rectangle(drawing, boundRect[i].tl(), boundRect[i].br(), color, 2);
-		//circle(drawing, centers[i], (int)radius[i], color, 2);
 	}
 	imshow("Contours", drawing);
 }
 
 int main()
 {
-	std::string image_path = samples::findFile("C:\\Study\\ANPR\\TrainingImages\\input\\Audi-A4-528794d.png");
+	std::string image_path = samples::findFile(ImageName);
 	std::cout << "Image path " << image_path << "\n";
 	Mat img = imread(image_path, IMREAD_COLOR);
 
@@ -56,42 +80,23 @@ int main()
 		return 1;
 	}
 
-	//imshow("Color image", img);
 	Mat _grayImage;
 	cvtColor(img, _grayImage, COLOR_RGB2GRAY);
 	imshow("Gray image", _grayImage);
 	cv::Size sz = _grayImage.size();
 	std::cout << "Width=" << sz.width << " height=" << sz.height;
 
-	//Mat img_bw = _grayImage > 128;
-	//imshow("B&W", img_bw);
-	//Mat FilteredImage;
-	//doFilter(_grayImage, FilteredImage);
+	Mat _resizedImage;
+	resize(_grayImage, _resizedImage, Size(200, 60));
 
 	Mat _thresholdImage;
-	//threshold(_grayImage, _thresholdImage, 190, 255, THRESH_BINARY + THRESH_OTSU);
-	adaptiveThreshold(_grayImage, _thresholdImage, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 25, 0);
+	adaptiveThreshold(_resizedImage, _thresholdImage, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 25, 0);
 	imshow("Threshold Image", _thresholdImage);
-	
-	//bitwise_not(_thresholdImage, _thresholdImage);
-	//imshow("Reversed Image", _thresholdImage);
 
-	Mat _element = getStructuringElement(MORPH_RECT, Size(2, 2));
+	Mat _connectedImage(_thresholdImage.size(), CV_8UC3);
+	doConnectedComponents(_thresholdImage);
 
-	//Mat _erodedImage;
-	//erode(_thresholdImage, _erodedImage, _element);
-	//imshow("Eroded image", _erodedImage);
-	//
-	Mat _dilatedImage;
-	dilate(_thresholdImage, _dilatedImage, _element);
-	imshow("Dilated image", _dilatedImage);
-
-	doContour(_thresholdImage);
 	int k = waitKey(0);
-//	if (k == 's')
-//	{
-//		imwrite("starry_night.png", img);
-//	}
 
 	return 0;
 }
